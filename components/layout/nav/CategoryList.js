b79719c696872b6d038d830aaa48af85042761d2
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-12 21:19:24 -1000

    Render categories

---
+++ components/layout/nav/CategoryList.js
@@ -0,0 +1,22 @@
+import CategoryListItem from './CategoryListItem';
+
+import styles from './CategoryList.module.css';
+
+const categories = [
+  { name: 'Toy Problems', slug: 'toy-problems'},
+  { name: 'Problem Solving patterns', slug: 'problem-solving-patterns'},
+  { name: 'Sorting Algorithms', slug: 'sorting-algorithms'},
+  { name: 'Data Structures', slug: 'data-structures'},
+]
+
+const CategoryList = () => {
+  return (
+    <div className={styles.container}>
+      {categories.map(cat => (
+        <CategoryListItem cat={cat} />
+      ))}
+    </div>
+  )
+};
+
+export default CategoryList;
