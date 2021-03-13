3bbae2197ba1506b34af7fd3fb96f7d1b928f26d
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-12 21:16:28 -1000

    Add Layout wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { Fragment } from 'react';
+
+import Layout from '../components/layout/Layout';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
