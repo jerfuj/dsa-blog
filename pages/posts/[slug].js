6d72fea8c43264e2021637e1822e96ae83b3e090
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-13 14:23:29 -1000

    Implement getStaticProps and getStaticPaths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,36 @@
+import PostDetails from '../../components/posts/PostDetails';
+import { getPostData, getPostFiles } from '../../lib/postHelpers';
 
-const FullPostPage = () => {
+
+const FullPostPage = ({ postData }) => {
   return (
-    <div>
-      Hello
-    </div>
+    <PostDetails postData={postData} />
   )
 };
+
+export const getStaticProps = ({ params }) => {
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export const getStaticPaths = () => {
+  const filenames = getPostFiles();
+
+  const slugs = filenames.map(name => (
+    name.replace(/\.md$/, '')
+  ));
+
+  return {
+    paths: slugs.map(slug => ({params: {slug}})),
+    fallback: false
+  }
+}
 
 export default FullPostPage;
