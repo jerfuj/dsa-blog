1f793c163c1daf53dd16f9c84c29ac36874dfe8d
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-13 14:21:41 -1000

    Add class to section

--- components/posts/RecentPosts.js
+++ components/posts/RecentPosts.js
@@ -3,8 +3,9 @@ import PostGrid from './PostGrid';
 import styles from './RecentPosts.module.css';
 
 const RecentPosts = ({ posts }) => {
+
   return (
-    <section>
+    <section className={styles.postsContainer}>
       <h2 className={styles.header}>Latest Posts</h2>
       <PostGrid posts={posts} />
     </section>
