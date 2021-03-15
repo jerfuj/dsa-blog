248c51d2d612678b3975bd9883480e570dc524a1
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-14 16:05:07 -1000

    Add data structures pages

---
+++ pages/data-structures/index.js
@@ -0,0 +1,26 @@
+import AllPosts from '../../components/posts/AllPosts';
+
+import { getPostData, getPostFiles } from '../../lib/postHelpers';
+
+export default function CategoryPage({ posts, category }) {
+  return (
+    <AllPosts posts={posts} category={category} />
+  )
+}
+
+export const getStaticProps = () => {
+  let posts = getPostFiles('data-structures');
+
+  posts = posts.map((post) => (
+    getPostData('data-structures', post)
+  ))
+
+  return {
+    props: {
+      posts,
+      category: 'Data Structures'
+    }
+  }
+}
+
+
