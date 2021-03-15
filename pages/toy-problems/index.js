339f80f2b8c66e0f8c4a5c09c5e3d9423faa50b4
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-14 16:05:55 -1000

    Add toy problems pages

---
+++ pages/toy-problems/index.js
@@ -0,0 +1,24 @@
+import AllPosts from '../../components/posts/AllPosts';
+
+import { getPostData, getPostFiles } from '../../lib/postHelpers';
+
+export default function CategoryPage({ posts, category }) {
+  return (
+    <AllPosts posts={posts} category={category} />
+  )
+}
+
+export const getStaticProps = () => {
+  let posts = getPostFiles('toy-problems');
+
+  posts = posts.map((post) => (
+    getPostData('toy-problems', post)
+  ))
+
+  return {
+    props: {
+      posts,
+      category: 'Toy Problems'
+    }
+  }
+}
