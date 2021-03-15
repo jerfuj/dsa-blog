af1d81415160a6ecf809517a1b07d45c0b167d6a
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-14 16:05:39 -1000

    Add sorting algorithms pages

---
+++ pages/sorting-algorithms/index.js
@@ -0,0 +1,26 @@
+import AllPosts from '../../components/posts/AllPosts';
+
+import { getPostData, getPostFiles } from '../../lib/postHelpers';
+
+export default function CategoryPage({ posts, category }) {
+  return (
+    <AllPosts posts={posts} category={category} />
+  )
+}
+
+export const getStaticProps = () => {
+  let posts = getPostFiles('sorting-algorithms');
+
+  posts = posts.map((post) => (
+    getPostData('sorting-algorithms', post)
+  ))
+
+  return {
+    props: {
+      posts,
+      category: 'Sorting Algorithms'
+    }
+  }
+}
+
+
