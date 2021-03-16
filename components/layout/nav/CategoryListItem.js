6237f308833a19e3426e49122633575831893cfe
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-15 15:19:40 -1000

    Add dropdown context

--- components/layout/nav/CategoryListItem.js
+++ components/layout/nav/CategoryListItem.js
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+import DropdownContext from '../../../context/DropdownContext';
+
 import Link from 'next/link';
 
 import styles from './CategoryListItem.module.css';
 
 const CategoryListItem = ({ cat }) => {
+  const dropdownCtx = useContext(DropdownContext);
+
   return (
-    <li className={styles.listItem}>
+    <li
+      className={styles.listItem}
+      onClick={dropdownCtx.closeDropdown}
+    >
       <Link href={`/${cat.slug}`}>{cat.name}</Link>
     </li>
   )
