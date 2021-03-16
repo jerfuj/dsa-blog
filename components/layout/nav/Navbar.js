1419caff42fa85821fb1b14f15ca62869861e2e5
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-15 15:19:58 -1000

    Add dropdown context

--- components/layout/nav/Navbar.js
+++ components/layout/nav/Navbar.js
@@ -1,3 +1,6 @@
+import { useState, useContext } from 'react';
+import DropdownContext from '../../../context/DropdownContext';
+
 import Link from 'next/link';
 
 import CategoryDropdown from './CategoryDropdown';
@@ -5,14 +8,18 @@ import CategoryDropdown from './CategoryDropdown';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const dropdownCtx = useContext(DropdownContext);
+
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <a>
-          <h1>DSA Blog</h1>
-        </a>
-      </Link>
-      <CategoryDropdown />
+      <div onClick={dropdownCtx.closeDropdown}>
+        <Link href="/">
+          <a>
+            <h1>DSA Blog</h1>
+          </a>
+        </Link>
+      </div>
+      <CategoryDropdown/>
     </header>
   )
 };
