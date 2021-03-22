f37a8a4befa5366e722b5f9ee3da88d356e42641
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-22 07:12:34 -1000

    Clean up and comment  for Airbnb application

--- components/layout/nav/CategoryList.js
+++ components/layout/nav/CategoryList.js
@@ -1,8 +1,6 @@
 import { useEffect, useContext, useRef } from 'react';
 import DropdownContext from '../../../context/DropdownContext';
-
 import CategoryListItem from './CategoryListItem';
-
 import styles from './CategoryList.module.css';
 
 const categories = [
@@ -10,32 +8,39 @@ const categories = [
   { name: 'Problem Solving patterns', slug: 'problem-solving-patterns'},
   { name: 'Sorting Algorithms', slug: 'sorting-algorithms'},
   { name: 'Data Structures', slug: 'data-structures'},
-]
+];
 
 const CategoryList = () => {
   const dropdownCtx = useContext(DropdownContext);
   const listRef = useRef();
 
-  useEffect(() => {
-    const handleOutsideClick = (e) => {
-      if (!listRef.current) {
-        document.removeEventListener('click', handleOutsideClick);
-      } else {
-        if (!listRef.current.contains(e.target)) {
-          dropdownCtx.closeDropdown();
-        }
-      }
+  /**
+   * handleOutsideClick is a dynamic click handler that is added to the document
+   * each time this component is mounted
+   * It allows the ability to close the menu by clicking anywhere outside of it
+   * The second conditional contains the main logic to closing the menu
+   * The first conditional is there in case the menu was already closed through a different method
+   */
+  const handleOutsideClick = (e) => {
+    if (!listRef.current) {
+      document.removeEventListener('click', handleOutsideClick);
+    } else if (!listRef.current.contains(e.target)) {
+      dropdownCtx.closeDropdown();
+      document.removeEventListener('click', handleOutsideClick);
     }
+  };
+
+  useEffect(() => {
     document.addEventListener('click', handleOutsideClick);
-  }, [])
+  }, []);
 
   return (
     <ul className={styles.container} ref={listRef}>
-      {categories.map((cat, i) => (
-        <CategoryListItem key={i} cat={cat} />
+      {categories.map((cat) => (
+        <CategoryListItem key={cat.slug} cat={cat} />
       ))}
     </ul>
-  )
+  );
 };
 
 export default CategoryList;
