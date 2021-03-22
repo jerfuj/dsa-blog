6ca8a0db093500bacdfbbaab7d29d056272197e6
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-22 07:17:07 -1000

    Remove Link import, clean with ESLint

--- components/posts/PostDetails.js
+++ components/posts/PostDetails.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/no-children-prop */
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import PropTypes from 'prop-types';
 
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism/';
@@ -9,9 +11,7 @@ import styles from './PostDetails.module.css';
 
 SyntaxHighlighter.registerLanguage('js', js);
 
-
 const PostDetails = ({ postData }) => {
-
   const customRenderers = {
     paragraph(paragraph) {
       const { node } = paragraph;
@@ -28,10 +28,10 @@ const PostDetails = ({ postData }) => {
               height={310}
             />
           </div>
-        )
+        );
       }
 
-      return <p>{paragraph.children}</p>
+      return <p>{paragraph.children}</p>;
     },
     code(code) {
       const { language, value } = code;
@@ -42,9 +42,9 @@ const PostDetails = ({ postData }) => {
           style={vscDarkPlus}
           className={styles.code}
         />
-      )
-    }
-  }
+      );
+    },
+  };
 
   return (
     <div className={styles.container}>
@@ -54,7 +54,11 @@ const PostDetails = ({ postData }) => {
       </ReactMarkdown>
 
     </div>
-  )
+  );
 };
 
 export default PostDetails;
+
+PostDetails.propTypes = {
+  postData: PropTypes.instanceOf(Object).isRequired,
+};
