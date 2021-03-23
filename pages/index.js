0c5cc5996bbab505608ede7a0db6a2f17c00aaa4
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-23 07:25:17 -1000

    Add getAllPosts to home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import PropTypes from 'prop-types';
 import Home from '../components/home/Home';
+import { getAllPosts } from '../lib/postHelpers';
 
-const HomePage = () => (
+const HomePage = ({ posts }) => (
   <Home />
 );
 
 export default HomePage;
+
+export const getStaticProps = () => {
+  const posts = getAllPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+HomePage.propTypes = {
+  posts: PropTypes.instanceOf(Array).isRequired,
+};
