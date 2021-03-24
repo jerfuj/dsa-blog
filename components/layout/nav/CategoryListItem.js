d59ce6d97e99f8b3ab3c1f0e4a307131200e042f
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-24 10:58:47 -1000

    ESlint cleanup

--- components/layout/nav/CategoryListItem.js
+++ components/layout/nav/CategoryListItem.js
@@ -1,8 +1,9 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import PropTypes from 'prop-types';
 import { useContext } from 'react';
-import DropdownContext from '../../../context/DropdownContext';
-
 import Link from 'next/link';
-
+import DropdownContext from '../../../context/DropdownContext';
 import styles from './CategoryListItem.module.css';
 
 const CategoryListItem = ({ cat }) => {
@@ -13,9 +14,15 @@ const CategoryListItem = ({ cat }) => {
       className={styles.listItem}
       onClick={dropdownCtx.closeDropdown}
     >
-      <Link href={`/${cat.slug}`}>{cat.name}</Link>
+      <Link href={`/${cat.slug}`}>
+        {cat.name}
+      </Link>
     </li>
-  )
+  );
 };
 
 export default CategoryListItem;
+
+CategoryListItem.propTypes = {
+  cat: PropTypes.instanceOf(Object).isRequired,
+};
