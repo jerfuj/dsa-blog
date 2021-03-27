721a9b1c6cc55bb914b7daa178a2376ad85a28bd
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-26 17:13:32 -1000

    Add head, title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+import Head from 'next/head';
 import Layout from '../components/layout/Layout';
 
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
   <Layout>
+    <Head>
+      <title>DSA Blog</title>
+      <link rel="shortcut icon" href="/favicon.png" />
+    </Head>
     <Component {...pageProps} />
   </Layout>
 );
