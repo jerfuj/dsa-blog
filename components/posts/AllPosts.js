29b5aa54436bb1ebd92d6536514901a30401ab7d
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-26 22:02:10 -1000

    Refactor and add proptypes

--- components/posts/AllPosts.js
+++ components/posts/AllPosts.js
@@ -1,15 +1,18 @@
+import PropTypes from 'prop-types';
 import PostGrid from './PostGrid';
 
 import styles from './AllPosts.module.css';
 
-const AllPosts = ({ posts, category }) => {
+const AllPosts = ({ posts, category }) => (
+  <section className={styles.postsContainer}>
+    <h2 className={styles.header}>{category}</h2>
+    <PostGrid posts={posts} />
+  </section>
+);
 
-  return (
-    <section className={styles.postsContainer}>
-      <h2 className={styles.header}>{category}</h2>
-      <PostGrid posts={posts} />
-    </section>
-  )
+AllPosts.propTypes = {
+  posts: PropTypes.instanceOf(Array).isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default AllPosts;
