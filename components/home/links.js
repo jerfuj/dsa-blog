f330017eab585f2b6d20b04368fbbaa5db9865cf
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-26 22:03:11 -1000

    Add links data

---
+++ components/home/links.js
@@ -0,0 +1,25 @@
+const links = [
+  {
+    id: 'js-dsa',
+    name: 'JavaScript Algorithms and Data Structures Masterclass',
+    url: 'https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/',
+    description: 'This course was exactly what I needed post-bootcamp. Colt is an amazing instructor and he takes you step by step through topics such as big O, problem solving approaches, sorting algorithms, and data structures.',
+    image: '/images/js-dsa.png',
+  },
+  {
+    id: 'visualgo',
+    name: 'VisuAlgo',
+    url: 'https://visualgo.net/en/sorting',
+    description: 'For visual learners, this site provides really nice and intuitive visuals for sorting algorithms and data structures.',
+    image: '/images/visualgo.png',
+  },
+  {
+    id: 'next-js',
+    name: 'Next.js & React - The Complete Guide',
+    url: 'https://www.udemy.com/course/nextjs-react-the-complete-guide/',
+    description: 'Not DSA-related, but worth mentioning because this blog was made as a result of taking this course. Maximillian is a great teacher, and this was easily the best Next.js content I could find.',
+    image: '/images/next-js.png',
+  },
+];
+
+export default links;
